3f711d0f656dc9b8f3db6e80f9eff335f1c3864e
wxz0530 <2792602900@qq.com>
2022-09-22 09:35:04 +0800

    A第一次提交

---
+++ CNN/ALEXNET.py
@@ -0,0 +1,49 @@
+import time
+import torch
+from torch import nn, optim
+import torchvision
+import d2lzh_pytorch as d2l
+
+class AlexNet(nn.Module):
+    def __init__(self):
+        super(AlexNet, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 96, 11, 4),  # in_channels, out_channels,kernel_size, stride, padding
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2),
+            nn.Conv2d(96, 256, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2),
+            nn.Conv2d(256, 384, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(384, 384, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(384, 256, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(256 * 5 * 5, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            # 输出层。由于这⾥使⽤Fashion-MNIST，所以⽤类别数为10，⽽⾮论⽂中的1000
+            nn.Linear(4096, 10),
+        )
+
+    def forward(self, img):
+        feature = self.conv(img)
+        output = self.fc(feature.view(img.shape[0], -1))
+        return output
+
+net = AlexNet()
+print(net)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size,resize=224)
+lr, num_epochs = 0.001, 5
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+d2l.train_ch5(net, train_iter, test_iter, batch_size, optimizer, device,num_epochs)
---
+++ CNN/DenseNet.py
@@ -0,0 +1,86 @@
+import time
+import torch
+from torch import nn, optim
+import d2lzh_pytorch as d2l
+import torch.nn.functional as F
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def conv_block(in_channels, out_channels):
+    blk = nn.Sequential(nn.BatchNorm2d(in_channels),
+                        nn.ReLU(),
+                        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1))#WXH不变
+    return blk
+"""nn.Sequential与nn.ModuleList的区别
+不同点1：nn.Sequential内部实现了forward函数，因此可以不用写forward函数，而nn.ModuleList则没有实现内部forward函数。
+不同点2：nn.Sequential可以使用OrderedDict对每层进行命名。
+不同点3：nn.Sequential里面的模块按照顺序进行排列的，所以必须确保前一个模块的输出大小和下一个模块的输入大小是一致的。而nn.ModuleList 并没有定义一个网络，它只是将不同的模块储存在一起，这些模块之间并没有什么先后顺序可言。
+不同点4：有的时候网络中有很多相似或者重复的层，我们一般会考虑用 for 循环来创建它们
+"""
+class DenseBlock(nn.Module):
+    def __init__(self, num_convs, in_channels, out_channels):
+        super(DenseBlock, self).__init__()
+        net = []
+        for i in range(num_convs):
+            in_c = in_channels + i * out_channels
+            net.append(conv_block(in_c, out_channels))
+        self.net = nn.ModuleList(net)
+        self.out_channels = in_channels + num_convs * out_channels # 计算输出通道数
+
+    def forward(self, X):
+        for blk in self.net:
+            Y = blk(X)
+            X = torch.cat((X, Y), dim=1)  # 在通道维上将输入和输出连结
+        return X
+
+blk = DenseBlock(2, 3, 10)
+X = torch.rand(4, 3, 8, 8)
+Y = blk(X)
+Y.shape#torch.Size([4, 23, 8, 8])
+
+def transition_block(in_channels, out_channels):#过度层 降维
+    blk = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1),
+            nn.AvgPool2d(kernel_size=2, stride=2))#两倍下采样
+    return blk
+
+blk = transition_block(23, 10)
+blk(Y).shape#torch.Size([4, 10, 4, 4])
+
+net = nn.Sequential(
+        nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+        nn.BatchNorm2d(64),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+num_channels, growth_rate = 64, 32  # num_channels为当前的通道数
+num_convs_in_dense_blocks = [4, 4, 4, 4]
+
+for i, num_convs in enumerate(num_convs_in_dense_blocks):
+    DB = DenseBlock(num_convs, num_channels, growth_rate)
+    net.add_module("DenseBlosk_%d" % i, DB)
+    # 上一个稠密块的输出通道数
+    num_channels = DB.out_channels
+    # 在稠密块之间加入通道数减半的过渡层
+    if i != len(num_convs_in_dense_blocks) - 1:
+        net.add_module("transition_block_%d" % i, transition_block(num_channels, num_channels // 2))
+        num_channels = num_channels // 2
+
+net.add_module("BN", nn.BatchNorm2d(num_channels))
+net.add_module("relu", nn.ReLU())
+net.add_module("global_avg_pool", d2l.GlobalAvgPool2d()) # GlobalAvgPool2d的输出: (Batch, num_channels, 1, 1)
+net.add_module("fc", nn.Sequential(d2l.FlattenLayer(), nn.Linear(num_channels, 10)))
+
+X = torch.rand((1, 1, 96, 96))
+for name, layer in net.named_children():
+    X = layer(X)
+    print(name, ' output shape:\t', X.shape)
+
+batch_size = 256
+# 如出现“out of memory”的报错信息，可减小batch_size或resize
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=96)
+
+lr, num_epochs = 0.001, 5
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+d2l.train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs)
